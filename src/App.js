d3d6c2383c927b658990fa13b91d4319a6473967
hanjunnn <qwedfr79@naver.com>
2024-02-16 15:03:22 +0900

    메인화면+판매화면

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import Buy from "./carrot/Buy";
 import Sell from "./carrot/Sell";
+import Carrot from "./Carrot";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Sell />}></Route>
+        <Route path="/" element={<Carrot />}></Route>
         <Route path="/Sell" element={<Sell />}></Route>
       </Routes>
     </div>
