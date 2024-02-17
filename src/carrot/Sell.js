b01cb5a4ccb494f53f0f79625fda8f0862272331
hanjunnn <qwedfr79@naver.com>
2024-02-17 15:15:56 +0900

    id문제해결

--- src/carrot/Sell.js
+++ src/carrot/Sell.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Sell.css";
 import { Link } from "react-router-dom";
 
 export default function Sell({ addProduct }) {
-  const [id, setId] = useState(1); // 상품 ID 상태 추가
+  const [id, setId] = useState(1); // 상품 ID 상태 추가 
   const [title, setTitle] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [imageFile, setImageFile] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
-
+ // useEffect를 사용하여 컴포넌트가 마운트될 때 고유한 ID 생성
+ useEffect(() => {
+  // 로컬 스토리지에서 최신 ID를 가져오거나 새로운 ID 생성
+  const latestId = parseInt(localStorage.getItem("latestId")) || 1;
+  setId(latestId);
+}, []);
+  
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     setImageFile(file);
@@ -29,6 +35,8 @@ export default function Sell({ addProduct }) {
 
     // 부모 컴포넌트로부터 전달받은 함수를 호출하여 상품 추가
     addProduct(newProduct);
+    // 최신 ID를 업데이트하고 로컬 스토리지에 저장
+    localStorage.setItem("latestId", String(id + 1));
     setId(id + 1);
   };
 
